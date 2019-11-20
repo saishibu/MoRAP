301752297d875e830945156e475a785bbdba542c
Sai Shibu N B <saishibu38380@gmail.com>
2019-11-20 17:09:28 +0530

    Update LORA_CLIENT.py

--- LORA_CLIENT.py
+++ LORA_CLIENT.py
@@ -1,98 +1,138 @@
-#!/usr/bin/python3
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@saishibu38380 
+20
+10457mayeranalytics/pySX127x
+ Code Issues 12 Pull requests 0 Actions Projects 0 Wiki Security Insights
+pySX127x/rx_cont.py / 
+@bjcarne bjcarne changes to get socket transceiver working
+c731fe2 on 17 Aug 2016
+@mcmayer@bjcarne
+Executable File  119 lines (96 sloc)  3.44 KB
+ 
+ You're using jump to definition to discover and navigate code.
+Learn more or give us feedback
+#!/usr/bin/env python3
+
+""" A simple continuous receiver class. """
+
+# Copyright 2015 Mayer Analytics Ltd.
+#
+# This file is part of pySX127x.
+#
+# pySX127x is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pySX127x is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You can be released from the requirements of the license by obtaining a commercial license. Such a license is
+# mandatory as soon as you develop commercial activities involving pySX127x without disclosing the source code of your
+# own applications, or shipping pySX127x with a closed source product.
+#
+# You should have received a copy of the GNU General Public License along with pySX127.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 
-import time
+from time import sleep
 from SX127x.LoRa import *
-#from SX127x.LoRaArgumentParser import LoRaArgumentParser
+from SX127x.LoRaArgumentParser import LoRaArgumentParser
 from SX127x.board_config import BOARD
 
 BOARD.setup()
-BOARD.reset()
-#parser = LoRaArgumentParser("Lora tester")
+
+parser = LoRaArgumentParser("Continous LoRa receiver.")
 
 
-def init(verbose=False):
-    #super(mylora, self).__init__(verbose)
-    LoRa.set_mode(MODE.SLEEP)
-    LoRa.set_dio_mapping([0] * 6)
-def on_rx_done(data):
-     BOARD.led_on()
-     #print("\nRxDone")
-     LoRa.clear_irq_flags(RxDone=1)
-     payload = LoRa.read_payload(nocheck=True )# Receive INF
-     print ("Receive: ")
-     mens=bytes(payload).decode("utf-8",'ignore')
-     mens=mens[2:-1] #to discard \x00\x00 and \x00 at the end
-     print(mens)
-     BOARD.led_off()
-#      if mens=="INF":
-     print("Received data request INF")
-     time.sleep(2)
-     print ("Send mens: DATA RASPBERRY PI")
-     LoRa.write_payload([255, 255, 0, 0, 68, 65, 84, 65, 32, 82, 65, 83, 80, 66, 69, 82, 82, 89, 32, 80, 73, 0]) # Send DATA RASPBERRY PI
-     LoRa.set_mode(MODE.TX)
-     time.sleep(2)
-     LoRa.reset_ptr_rx()
-     LoRa.set_mode(MODE.RXCONT)
-def on_tx_done(self):
-     print("\nTxDone")
-     print(self.get_irq_flags())
-def on_cad_done(self):
-     print("\non_CadDone")
-     print(self.get_irq_flags())
-def on_rx_timeout(self):
-     print("\non_RxTimeout")
-     print(self.get_irq_flags())
-def on_valid_header(self):
-     print("\non_ValidHeader")
-     print(self.get_irq_flags())
-def on_payload_crc_error(self):
-     print("\non_PayloadCrcError")
-     print(self.get_irq_flags())
+class LoRaRcvCont(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaRcvCont, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0] * 6)
 
-def on_fhss_change_channel(self):
-     print("\non_FhssChangeChannel")
-     print(self.get_irq_flags())
-#  def start():
-#     while True:
-#         LoRa.reset_ptr_rx()
-#         LoRa.set_mode(MODE.RXCONT) # Receiver mode
-#         while True:
-#             pass;
-            
+    def on_rx_done(self):
+        BOARD.led_on()
+        print("\nRxDone")
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        print(bytes(payload).decode())
+        self.set_mode(MODE.SLEEP)
+        self.reset_ptr_rx()
+        BOARD.led_off()
+        self.set_mode(MODE.RXCONT)
 
-#args = parser.parse_args(lora) # configs in LoRaArgumentParser.py
-LoRa=LoRa()
-#     Slow+long range  Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. 13 dBm
-init(False)
-#LoRa.set_pa_config(pa_select=1, max_power=21, output_power=15)
-# LoRa.set_bw(BW.BW125)
-# LoRa.set_coding_rate(CODING_RATE.CR4_8)
-# LoRa.set_spreading_factor(12)
-# LoRa.set_rx_crc(True)
-# LoRa.reset_ptr_rx()
-# LoRa.set_mode(MODE.RXCONT)
+    def on_tx_done(self):
+        print("\nTxDone")
+        print(self.get_irq_flags())
+
+    def on_cad_done(self):
+        print("\non_CadDone")
+        print(self.get_irq_flags())
+
+    def on_rx_timeout(self):
+        print("\non_RxTimeout")
+        print(self.get_irq_flags())
+
+    def on_valid_header(self):
+        print("\non_ValidHeader")
+        print(self.get_irq_flags())
+
+    def on_payload_crc_error(self):
+        print("\non_PayloadCrcError")
+        print(self.get_irq_flags())
+
+    def on_fhss_change_channel(self):
+        print("\non_FhssChangeChannel")
+        print(self.get_irq_flags())
+
+    def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+        while True:
+            sleep(.5)
+            rssi_value = self.get_rssi_value()
+            status = self.get_modem_status()
+            sys.stdout.flush()
+            sys.stdout.write("\r%d %d %d" % (rssi_value, status['rx_ongoing'], status['modem_clear']))
+
+
+lora = LoRaRcvCont(verbose=False)
+args = parser.parse_args(lora)
+
+lora.set_mode(MODE.STDBY)
+lora.set_pa_config(pa_select=1)
+#lora.set_rx_crc(True)
+#lora.set_coding_rate(CODING_RATE.CR4_6)
+#lora.set_pa_config(max_power=0, output_power=0)
 #lora.set_lna_gain(GAIN.G1)
 #lora.set_implicit_header_mode(False)
-# LoRa.set_low_data_rate_optim(True)
+#lora.set_low_data_rate_optim(True)
+#lora.set_pa_ramp(PA_RAMP.RAMP_50_us)
+#lora.set_agc_auto_on(True)
 
-#  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
-#lora.set_pa_config(pa_select=1)
+print(lora)
+assert(lora.get_agc_auto_on() == 1)
 
-
-data=[255,255,0,0,55,54,46,50,0]
-assert(LoRa.get_agc_auto_on() == 1)
+try: input("Press enter to start...")
+except: pass
 
 try:
-    print("START")
-#     start()
-    on_rx_done(data)
+    lora.start()
 except KeyboardInterrupt:
     sys.stdout.flush()
-    print("Exit")
+    print("")
     sys.stderr.write("KeyboardInterrupt\n")
 finally:
     sys.stdout.flush()
-    print("Exit")
-    LoRa.set_mode(MODE.SLEEP)
-BOARD.teardown()
+    print("")
+    lora.set_mode(MODE.SLEEP)
+    print(lora)
+    BOARD.teardown()
