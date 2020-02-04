69e269e591e25426d16e38787956e3cc46e04e01
Sai Shibu N B <saishibu38380@gmail.com>
2020-02-04 12:08:53 +0530

    Create MQTTSubPi.py

---
+++ MQTTSubPi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+import os
+#botUpdate
+import time
+import paho.mqtt.client as paho
+import datetime
+
+global mqttclient;
+global broker;
+global port;
+broker = "0.0.0.0";
+port = 1883;
+# This is the Subscriber
+mypid = os.getpid()
+print("Process started at: " +str(mypid))
+client_uniq = "pubclient_"+str(mypid)
+mqttclient = paho.Client(client_uniq, False) #nocleanstart
+mqttclient.connect(broker, port, 60)
+mqttclient.subscribe("MORAP/#")
+
+def test(client, userdata, message):
+	payload=message.payload.decode()
+	print(payload)
+
+mqttclient.message_callback_add("MORAP/test", test)
+
+
+mqttclient.loop_forever()
+
+
+
