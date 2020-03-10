1e4681c510ba5d0f2490a448c57ae69a487b29b3
menondivya <52484878+menondivya@users.noreply.github.com>
2020-03-10 12:15:24 +0530

    Update MQTTPub.py

--- MQTTPub.py
+++ MQTTPub.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
-import json
+import Adafruit_DHT
 
+import json
+gpio=17
+  
 def publish(id,data,topic):
 # This is the Publisher
   broker="34.93.40.246"
@@ -12,3 +15,11 @@ def publish(id,data,topic):
   client.connect(broker,port,0)
   (rc,mid)=client.publish(topic,payload);
   client.disconnect();
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+if humidity is not None and temperature is not None:
+  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+  dht_data = 
+  publish(DHT, dht_data, 'CCS\test')
+else:
+  print('Failed to get reading. Try again!')
