e2227225d5dbffd423b27af7ce948ae1aa6f19e9
Sai Shibu N B <saishibu38380@gmail.com>
2020-03-10 11:41:39 +0530

    Update MQTTPub.py

--- MQTTPub.py
+++ MQTTPub.py
@@ -1,13 +1,14 @@
 import paho.mqtt.client as mqtt
 import json
 
+def publish(id,data,topic):
 # This is the Publisher
-broker="34.93.40.246"
-port=1883
-topic="CCS\test"
-payload={'message':"hello"}
-payload=json.dumps(payload)
-client = mqtt.Client()
-client.connect(broker,port,0)
-(rc,mid)=client.publish(topic,payload);
-client.disconnect();
+  broker="34.93.40.246"
+  port=1883
+  #topic="CCS\test"
+  payload={'id':str(id),'data':data}
+  payload=json.dumps(payload)
+  client = mqtt.Client()
+  client.connect(broker,port,0)
+  (rc,mid)=client.publish(topic,payload);
+  client.disconnect();
