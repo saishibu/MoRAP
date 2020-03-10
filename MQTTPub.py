8c12f7765c66ab2411aea111cdf0ea44bab0bb76
menondivya <52484878+menondivya@users.noreply.github.com>
2020-03-10 12:22:41 +0530

    Update MQTTPub.py

--- MQTTPub.py
+++ MQTTPub.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 import Adafruit_DHT
-
 import json
+
 gpio=17
   
 def publish(id,data,topic):
@@ -12,14 +12,20 @@ def publish(id,data,topic):
   payload={'id':str(id),'data':data}
   payload=json.dumps(payload)
   client = mqtt.Client()
-  client.connect(broker,port,0)
+  client.connect(broker,port,60)
   (rc,mid)=client.publish(topic,payload);
   client.disconnect();
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 if humidity is not None and temperature is not None:
-  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-  dht_data = 
-  publish(DHT, dht_data, 'CCS\test')
+  StaticJsonBuffer<200> JSONBuffer;
+  JsonObject& root = JSONBuffer.createObject();
+  root["Temperature"] = t;
+  root["Humidity"] = h;
+  char JSONmessageBuffer[100];
+  root.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  JsonArray& data = root.createNestedArray("data");
+  print(JSONmessageBuffer);
+  publish(DHT, JSONmessageBuffer, CCS\test)
 else:
   print('Failed to get reading. Try again!')
