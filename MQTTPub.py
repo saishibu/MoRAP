d4eab2b184fbbe26464fb1420ccf04e90fa0e9da
Sai Shibu N B <saishibu38380@gmail.com>
2020-03-10 10:42:20 +0530

    Create MQTTPub.py

---
+++ MQTTPub.py
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+import json
+
+# This is the Publisher
+broker="34.93.40.246"
+port=1883
+topic="CCS\test"
+payload={'message':payload}
+payload=json.dumps(payload)
+client = mqtt.Client()
+client.connect(broker,port,0)
+(rc,mid)=client.publish(topic,payload);
+client.disconnect();
