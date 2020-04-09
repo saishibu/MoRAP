01ba5665ef3b9937f4ddc69d575c50171b7abaaf
Sai Shibu N B <saishibu38380@gmail.com>
2020-04-09 13:39:20 +0530

    Update TSPFuzzyCostExample.py

--- TSPFuzzyCostExample.py
+++ TSPFuzzyCostExample.py
@@ -1,11 +1,21 @@
 import TSPFuzzyCost as fuzzy
-#Install SKFUZZY --> pip3 install scikit-fuzzy
 
-flame=3
-pir=1
-distance=50
-temp=30
+#Sensor Values
+distance=500
+difficulty=100
+condition=100
 
-cost=fuzzy.getCost(flame,pir,distance,temp)
+#Pass it to the Fuzzy function
+cost=fuzzy.getCost(distance,difficulty,condition)
+
 print("Weight for TSP:")
 print(cost)
+
+#use the obtained cost in TSP
+
+#Test case:
+#1) Dis=500,diff=100,cond=100: cost=1
+#2) Dis=20,diff=90,cond=60: cost=5
+#3) Dis=400,diff=30,cond=70: cost=4
+#4) Dis=30,diff=10,cond=5: cost=6
+#5) Dis=250,diff=45,cond=80: cost=5
